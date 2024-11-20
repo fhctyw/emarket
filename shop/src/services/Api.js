4441a8582170e7361790902e73e1a79990bb950d
fhctyw <lisnychukarsenw@gmail.com>
2024-11-20 23:36:38 +0200

    add frontend dynamic api host

--- shop/src/services/Api.js
+++ shop/src/services/Api.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 import store from "../store";
 
+if (!process.env.API_HOST) {
+  throw new Error("Environment variable API_HOST is not defined!");
+}
+
+const API_HOST = process.env.API_HOST;
+
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8081`,
+    baseURL: `http://${API_HOST}:8081`,
     headers: {
       Authorization: `Bearer ${store.state.CurrentUser.token}`,
     },
